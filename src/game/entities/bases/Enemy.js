3e9a4439c762ec710661bc1492e5cd8e6c26c8ae
Bruno Assarisse <bruno@assarisse.com.br>
2014-01-25 11:59:01 -0200

    Removing unnecessary code.

--- src/game/entities/bases/Enemy.js
+++ src/game/entities/bases/Enemy.js
@@ -1,11 +1,6 @@
 
 var Enemy = Actor.extend({
 
-    _walkForceModifier: 1,
-    _directionChangeTime: kEnemyDirectionChangeTime,
-    _directionChangeIdleTime: 0,
-    _directionChangeMaxIdleTime: 0,
-    _lastMovingState: MovingState.Right,
     player: null,
 	state: EnemyState.Defence,
 	
@@ -51,50 +46,9 @@ var Enemy = Actor.extend({
     _setProperties: function(properties) {
         this._super(properties);
 
-        var pDirection = properties["direction"];
-        var pSpeed = properties["speed"];
-        var pIdleTime = properties["idleTime"];
-
-        this.movingState = pDirection == "left" ? MovingState.Left : MovingState.Right;
-
-        if (pSpeed)
-            this._walkForceModifier = parseFloat(pSpeed);
-        if (pIdleTime)
-            this._directionChangeMaxIdleTime = parseFloat(pIdleTime);
-
-    },
-
-    _changeDirection: function(idleTime) {
-
-        var directionMovingState = this.movingState == MovingState.Stopped ? this._lastMovingState : this.movingState;
-
-        if (idleTime <= 0)
-            this.movingState = directionMovingState == MovingState.Right ? MovingState.Left : MovingState.Right;
-        else {
-            this._lastMovingState = directionMovingState;
-            this.movingState = MovingState.Stopped;
-        }
-        this._directionChangeTime = kEnemyDirectionChangeTime;
-        this._directionChangeIdleTime = idleTime;
-
     },
 
     update: function(delta) {
-
-        if (this._directionChangeIdleTime > 0)
-            this._directionChangeIdleTime -= delta;
-
-        if (this._directionChangeTime <= 0 && this._directionChangeIdleTime <= 0) {
-
-            var velX = this.b2body.GetLinearVelocity().get_x();
-            var margin = 1 * this._walkForceModifier;
-
-            if (velX > -margin && velX < margin)
-                this._changeDirection(this.movingState == MovingState.Stopped ? 0 : this._directionChangeMaxIdleTime);
-
-        } else {
-            this._directionChangeTime -= delta;
-        }
 
         this.reactToPlayer();
 
